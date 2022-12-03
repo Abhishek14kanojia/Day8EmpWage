f48638b3546f7aa35f3cc3de340f4274864c0cc1
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-03 20:48:52 +0530

    Welcome to Employee Wage Computation Program UC2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,15 +2,26 @@ package com.day8EmpWage;
 
 public class EmployeeWage {
 
-	public void Attendance() {
+	int WagePerHours = 20;
+	int FullDayHours = 0;
+	int Wage = 0;
+	int FullTime = 1;
+	
+	public void DailyWage() {
 		
-		int empAttendance = (int) (Math.random()*5)%2;
-		if(empAttendance == 1) {
+		int empWage = (int)(Math.floor (Math.random()*5)%2);
+		if(empWage == FullTime) {
 			System.out.println("Employee is Present");
+			FullDayHours = 8;
+			Wage = FullDayHours * WagePerHours;
 		}else {
 			System.out.println("Employee is Absent");
+			FullDayHours = 0;
+			Wage = FullDayHours * WagePerHours;
 		}
-	
+		
+		System.out.println("Employee wage is : " + Wage + "Rupees");
+
 	}
 	
 }
