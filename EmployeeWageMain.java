003b69878c3e2b0dd64a61fab0fe1955f98c0c25
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-03 20:10:26 +0530

    Welcome to Employee Wage Computation Program

---
+++ EmployeeWageMain.java
@@ -0,0 +1,9 @@
+package com.day8EmpWage;
+
+public class EmployeeWageMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+}
