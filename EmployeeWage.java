eb913a3b48736a484149ff38001290d00bdc942a
Abhishek Kanojia <abhishek14kanojia@gmail.com>
2022-12-04 13:56:14 +0530

    Welcome to Employee Wage Computation Program UC3

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,21 +6,30 @@ public class EmployeeWage {
 	int FullDayHours = 0;
 	int Wage = 0;
 	int FullTime = 1;
+	int PartTime = 2;
+	int PartTimeHours = 0;
 	
 	public void DailyWage() {
 		
-		int empWage = (int)(Math.floor (Math.random()*5)%2);
+		int empWage = (int)(Math.floor (Math.random()*5)%3);
 		if(empWage == FullTime) {
 			System.out.println("Employee is Present");
 			FullDayHours = 8;
 			Wage = FullDayHours * WagePerHours;
+			System.out.println("Employee wage is : " + Wage + "Rupees");
+		}else if (empWage == PartTime){
+			System.out.println("Employee is Present in Part time ");
+			PartTimeHours = 4;
+			Wage = PartTimeHours * WagePerHours;
+			System.out.println("Employee wage is : " + Wage + "Rupees");
 		}else {
 			System.out.println("Employee is Absent");
 			FullDayHours = 0;
 			Wage = FullDayHours * WagePerHours;
+			System.out.println("Employee wage is : " + Wage + "Rupees");
+
 		}
 		
-		System.out.println("Employee wage is : " + Wage + "Rupees");
 
 	}
 	
